9f0b2eb49a66aea21b11a2b4c610240b2f9f86a6
Roddy <rd.mcn@ccgapps.net.au>
2024-08-27 18:06:28 +1000

    Change the timing away from the frame time delta to use time of day and ensure a transition will still play out if time has been taken away from the screen updates.

--- transition_handler.c
+++ transition_handler.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+#include <sys/time.h>
 
 #include "transition_handler.h"
 
@@ -24,6 +25,8 @@ static Image start_screen;
 static Texture2D end_screen;
 static RenderTexture2D screen_texture;
 
+static struct timeval transition_start_time;
+
 static void (*current_transition)(void); 
 static void init_fade(void);
 static void run_fade(void);
@@ -36,7 +39,10 @@ static void run_slide_left(void);
 static void init_slide_right(void);
 static void run_slide_right(void);
 
-static void end_transition();
+static void end_transition(void);
+
+static void set_transition_start_time(void);
+static double get_transition_time_delta(void);
 
 
 void set_transition_duration(float duration)
@@ -127,21 +133,31 @@ static void init_fade(void)
     data.end_texture = end_screen;
     data.start_value = 255;
     data.end_value = 0;
+
+    set_transition_start_time();
 }
 
 static void run_fade(void)
 {
     Color start_tint = WHITE;
     Color end_tint = WHITE;
-    float time_delta = GetFrameTime();
+    float time_delta = (float)get_transition_time_delta();
     float variance = (255 / (data.duration / time_delta));
+    float start_variance = data.start_value - variance;
+    float end_variance = data.end_value + variance;
+
+    if (start_variance < 0.0f || end_variance > 255.0f)
+    {
+        end_transition();
+        return;
+    }
+
+    start_tint.a = (unsigned char)start_variance;
+    end_tint.a = (unsigned char)end_variance;
 
     // RenderTextures have an opposite Y axis
     Rectangle rect_end_source = (Rectangle){ 0, 0, (float)data.end_texture.width, -(float)data.end_texture.height };
     Rectangle rect_end_dest = (Rectangle){ 0, 0, (float)data.end_texture.width, (float)data.end_texture.height };
-
-    start_tint.a = (unsigned char)data.start_value;
-    end_tint.a = (unsigned char)data.end_value;
 
     BeginDrawing();
         ClearBackground(BLACK);
@@ -149,14 +165,6 @@ static void run_fade(void)
         DrawTexture(data.start_texture, 0, 0, start_tint);
         DrawTexturePro(data.end_texture, rect_end_source, rect_end_dest, (Vector2){ 0, 0 }, 0.0f, end_tint);
     EndDrawing();
-
-    data.start_value -= variance;
-    data.end_value += variance;
-
-    if (data.start_value < 0.0f || data.end_value > 255.0f)
-    {
-        end_transition();
-    }
 }
 
 static void init_slide_left_overlap(void)
@@ -168,13 +176,21 @@ static void init_slide_left_overlap(void)
     data.end_texture = end_screen;
     data.start_value = (float)GetScreenWidth() * -1.0f;
     data.end_value = 0.0f;
+
+    set_transition_start_time();
 }
 
 static void run_slide_left_overlap(void)
 {
-    float time_delta = GetFrameTime();
+    float time_delta = (float)get_transition_time_delta();
     float variance = (GetScreenWidth() / (data.duration / time_delta));
-    float x = data.start_value;
+    float start_variance = data.start_value + variance;
+
+    if (start_variance >= data.end_value)
+    {
+        end_transition();
+        return;
+    }
 
     // RenderTextures have an opposite Y axis
     Rectangle rect_end_source = (Rectangle){ 0, 0, (float)data.end_texture.width, -(float)data.end_texture.height };
@@ -184,15 +200,8 @@ static void run_slide_left_overlap(void)
         ClearBackground(BLACK);
 
         DrawTexture(data.start_texture, 0, 0, WHITE);
-        DrawTexturePro(data.end_texture, rect_end_source, rect_end_dest, (Vector2){ x, 0 }, 0.0f, WHITE);
+        DrawTexturePro(data.end_texture, rect_end_source, rect_end_dest, (Vector2){ start_variance, 0 }, 0.0f, WHITE);
     EndDrawing();
-
-    data.start_value += variance;
-
-    if (data.start_value >= data.end_value)
-    {
-        end_transition();
-    }
 }
 
 static void init_slide_right_overlap(void)
@@ -204,13 +213,21 @@ static void init_slide_right_overlap(void)
     data.end_texture = end_screen;
     data.start_value = (float)GetScreenWidth();
     data.end_value = 0.0f;
+
+    set_transition_start_time();
 }
 
 static void run_slide_right_overlap(void)
 {
-    float time_delta = GetFrameTime();
+    float time_delta = (float)get_transition_time_delta();
     float variance = (GetScreenWidth() / (data.duration / time_delta));
-    float x = data.start_value;
+    float start_variance = data.start_value - variance;
+
+    if (start_variance <= data.end_value)
+    {
+        end_transition();
+        return;
+    }
 
     // RenderTextures have an opposite Y axis
     Rectangle rect_end_source = (Rectangle){ 0, 0, (float)data.end_texture.width, -(float)data.end_texture.height };
@@ -220,15 +237,8 @@ static void run_slide_right_overlap(void)
         ClearBackground(BLACK);
 
         DrawTexture(data.start_texture, 0, 0, WHITE);
-        DrawTexturePro(data.end_texture, rect_end_source, rect_end_dest, (Vector2){ x, 0 }, 0.0f, WHITE);
+        DrawTexturePro(data.end_texture, rect_end_source, rect_end_dest, (Vector2){ start_variance, 0 }, 0.0f, WHITE);
     EndDrawing();
-
-    data.start_value -= variance;
-
-    if (data.start_value <= data.end_value)
-    {
-        end_transition();
-    }
 }
 
 static void init_slide_left(void)
@@ -240,15 +250,25 @@ static void init_slide_left(void)
     data.end_texture = end_screen;
     data.start_value = (float)GetScreenWidth() * -1.0f;
     data.end_value = 0.0f;
+
+    set_transition_start_time();
 }
 
 static void run_slide_left(void)
 {
     int width = GetScreenWidth();
-    float time_delta = GetFrameTime();
+    float time_delta = (float)get_transition_time_delta();
     float variance = ((float)width / (data.duration / time_delta));
-    float start_x = ((float)width * -1.0f) - data.start_value;
-    float end_x = data.start_value;
+    float start_variance = data.start_value + variance;
+
+    if (start_variance >= data.end_value)
+    {
+        end_transition();
+        return;
+    }
+
+    float start_x = ((float)width * -1.0f) - start_variance;
+    float end_x = start_variance;
 
     // RenderTextures have an opposite Y axis
     Rectangle rect_end_source = (Rectangle){ 0, 0, (float)data.end_texture.width, -(float)data.end_texture.height };
@@ -260,13 +280,6 @@ static void run_slide_left(void)
         DrawTexture(data.start_texture, start_x, 0, WHITE);
         DrawTexturePro(data.end_texture, rect_end_source, rect_end_dest, (Vector2){ end_x, 0 }, 0.0f, WHITE);
     EndDrawing();
-
-    data.start_value += variance;
-
-    if (data.start_value >= data.end_value)
-    {
-        end_transition();
-    }
 }
 
 static void init_slide_right(void)
@@ -278,15 +291,25 @@ static void init_slide_right(void)
     data.end_texture = end_screen;
     data.start_value = (float)GetScreenWidth();
     data.end_value = 0.0f;
+
+    set_transition_start_time();
 }
 
 static void run_slide_right(void)
 {
     int width = GetScreenWidth();
-    float time_delta = GetFrameTime();
+    float time_delta = (float)get_transition_time_delta();
     float variance = ((float)width / (data.duration / time_delta));
-    float start_x = (float)width - data.start_value;
-    float end_x = data.start_value;
+    float start_variance = data.start_value - variance;
+
+    if (start_variance <= data.end_value)
+    {
+        end_transition();
+        return;
+    }
+
+    float start_x = (float)width - start_variance;
+    float end_x = start_variance;
 
     // RenderTextures have an opposite Y axis
     Rectangle rect_end_source = (Rectangle){ 0, 0, (float)data.end_texture.width, -(float)data.end_texture.height };
@@ -298,16 +321,9 @@ static void run_slide_right(void)
         DrawTexture(data.start_texture, start_x, 0, WHITE);
         DrawTexturePro(data.end_texture, rect_end_source, rect_end_dest, (Vector2){ end_x, 0 }, 0.0f, WHITE);
     EndDrawing();
-
-    data.start_value -= variance;
-
-    if (data.start_value <= data.end_value)
-    {
-        end_transition();
-    }
 }
 
-static void end_transition()
+static void end_transition(void)
 {
     UnloadImage(start_screen);
     UnloadTexture(end_screen);
@@ -318,4 +334,23 @@ static void end_transition()
     UnloadRenderTexture(screen_texture);
 
     transition_active = false;
+}
+
+static void set_transition_start_time(void)
+{
+    gettimeofday(&transition_start_time, NULL);
+}
+
+static double get_transition_time_delta(void)
+{
+    struct timeval transition_end_time;
+    double delta_usecs;
+    double delta;
+
+    gettimeofday(&transition_end_time, NULL);
+
+    delta_usecs = (double)(transition_end_time.tv_usec - transition_start_time.tv_usec) / 1000000.0;
+    delta = (double)(transition_end_time.tv_sec - transition_start_time.tv_sec) + delta_usecs;
+
+    return delta;
 }
